39ed60f2e7d33e9997edf3359a21bd8b3d08b783
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-05-16 23:50:48 +0530

    Create 2331_Evaluate_Boolean_Binary_Tree.java

---
+++ 2331. Evaluate Boolean Binary Tree/2331_Evaluate_Boolean_Binary_Tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if (root == null) return false;
+        
+        if (root.left == null && root.right == null) return root.val == 1;
+        
+        boolean leftValue = evaluateTree(root.left);
+        boolean rightValue = evaluateTree(root.right);
+        
+        if (root.val == 2) return leftValue || rightValue;
+        else return leftValue && rightValue;
+    }
+}
