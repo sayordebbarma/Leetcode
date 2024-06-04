f457028a272dbfbe2c0c5ec9e03deb36eb2cccd9
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-06-04 10:48:15 +0530

    Create 0409_Longest_Palindrome.java

---
+++ 0409. Longest Palindrome/0409_Longest_Palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        
+        int length = 0;
+        boolean oddFound = false;
+
+        for (int count : charCount.values()) {
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                oddFound = true;
+            }
+        }
+        
+        if (oddFound) length += 1;
+        
+        return length;
+    }
+}
