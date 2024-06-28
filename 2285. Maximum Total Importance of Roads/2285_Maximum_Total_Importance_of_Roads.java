dacff106d89d7c0ece2bd670c912240f9f6d3f82
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-06-28 22:21:26 +0530

    Create 2285_Maximum_Total_Importance_of_Roads.java

---
+++ 2285. Maximum Total Importance of Roads/2285_Maximum_Total_Importance_of_Roads.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long maximumImportance(int n, int[][] roads) {
+        int[] degree = new int[n];
+        for (int[] road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+
+        Integer[] cities = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            cities[i] = i;
+        }
+        Arrays.sort(cities, (a, b) -> degree[b] - degree[a]);
+
+        int[] values = new int[n];
+        for (int i = 0; i < n; i++) {
+            values[cities[i]] = n - i;
+        }
+
+        long totalImportance = 0;
+        for (int[] road : roads) {
+            totalImportance += values[road[0]] + values[road[1]];
+        }
+
+        return totalImportance;   
+    }
+}
