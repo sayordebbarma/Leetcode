87daed0b82d491ea361589877205e1980e406854
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-07-29 19:49:25 +0530

    Create 1395_Count_Number_of_Teams.java

---
+++ 1395. Count Number of Teams/1395_Count_Number_of_Teams.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        int n = rating.length;
+        int count = 0;
+
+        for (int j = 1; j < n - 1; j++) {
+            int leftLess = 0, leftGreater = 0;
+            int rightLess = 0, rightGreater = 0;
+
+            for (int i = 0; i < j; i++) {
+                if (rating[i] < rating[j]) leftLess++;
+                else if (rating[i] > rating[j]) leftGreater++;
+            }
+
+            for (int k = j + 1; k < n; k++) {
+                if (rating[k] < rating[j]) rightLess++;
+                else if (rating[k] > rating[j]) rightGreater++;
+            }
+
+            count += leftLess * rightGreater + leftGreater * rightLess;
+        }
+
+        return count;
+    }
+}
