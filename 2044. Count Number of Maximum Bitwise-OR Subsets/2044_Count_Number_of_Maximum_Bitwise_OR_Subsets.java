179fc8851e71db055091a5e8a3d20d342c7c85a0
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-10-18 21:31:02 +0530

    Create 2044_Count_Number_of_Maximum_Bitwise_OR_Subsets.java

---
+++ 2044. Count Number of Maximum Bitwise-OR Subsets/2044_Count_Number_of_Maximum_Bitwise_OR_Subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOr = 0;
+        for (int num : nums) {
+            maxOr |= num;
+        }
+        
+        return countSubsets(nums, 0, 0, maxOr);
+    }
+    
+    private static int countSubsets(int[] nums, int index, int currentOr, int maxOr) {
+        if (index == nums.length) return currentOr == maxOr ? 1 : 0;
+
+        int include = countSubsets(nums, index + 1, currentOr | nums[index], maxOr);
+        int exclude = countSubsets(nums, index + 1, currentOr, maxOr);
+        
+        return include + exclude;
+    }
+}
